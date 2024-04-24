aed698fce636c9407182676f6b359d5e49aebe05
SujitSingh521 <5215ssingh@gmail.com>
2024-04-24 18:24:24 +0530

    react_project_day-2

---
+++ Counter/react_project_day-2/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Counter from "./component/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Counter Component</h1>
+      
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ Counter/react_project_day-2/src/component/Counter.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+function Counter() 
+{
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+     setCount(count + 1);
+  };
+  const decrement = () => {
+     setCount(count - 1);
+  };
+
+  const reset = () => {
+      setCount(0);
+  };
+
+ 
+
+  return (
+    <div className="container">
+     <div className="counter">
+      <h1>Counter</h1>
+      <h2 className="count">{count}</h2>
+      <div className="buttons">
+
+      <button onClick={increment} id="btn1" >Increment</button>
+        <button onClick={decrement} id="btn2">Decrement</button>
+        <button onClick={reset} id="btn3">Reset</button>
+
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default Counter;
