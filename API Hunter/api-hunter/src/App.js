a8bbce0aa653643eb5f3ef771849f9f2fff389f4
SujitSingh521 <5215ssingh@gmail.com>
2024-07-06 02:09:06 +0530

    api-hunter

---
+++ API Hunter/api-hunter/src/App.js
@@ -0,0 +1,13 @@
+import AllRoutes from "./Routes/AllRoutes";
+import './App.css'
+
+ function App() {
+  return (
+    <div className="App">
+    <h1 style={{color:"teal",textAlign:"center"}}>API Hunter</h1>
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
