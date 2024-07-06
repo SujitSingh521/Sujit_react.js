22185ad23a054058deac32e0ab504876c89822aa
SujitSingh521 <5215ssingh@gmail.com>
2024-07-07 01:17:30 +0530

    Your message

---
+++ Data Table/data-table/src/AllRoutes.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import AddProduct from './components/AddProduct'
+import EditProduct from './components/EditProduct'
+import {Route, Routes} from 'react-router-dom'
+import ProductDetails from './components/ProductDetails'
+import ProductList from './components/ProductList'
+import DeleteProduct from './components/DeleteProduct'
+
+
+const AllRoutes = () => {
+  return (
+    <div>
+         <Routes>
+            <Route path='/ProductList' element={<ProductList/>}></Route>
+            <Route path='/ProductDetails/:id' element={<ProductDetails/>}></Route>
+            <Route path='/Addproduct' element={<AddProduct/>}></Route>
+            <Route path='/Editproduct/:id' element={<EditProduct/>}></Route>
+            <Route path='/Deleteproduct/:id' element={<DeleteProduct/>}></Route>
+            
+
+        </Routes>
+    </div>
+  )
+}
+
+export default AllRoutes
---
+++ Data Table/data-table/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import AllRoutes from './AllRoutes'
+import {Link, useParams} from 'react-router-dom'
+import './App.css'
+
+const App =() => {
+
+  return (
+    <div>
+      <div className='navbar'> 
+        <Link to={'/ProductList'} style={{textDecoration:"none",color:"white"}}>ProductList</Link>
+        <Link to={'/ProductDetails'} style={{textDecoration:"none",color:"white"}}>ProductDetails</Link>
+        <Link to={'/Addproduct'} style={{textDecoration:"none",color:"white"}}>Addproduct</Link>
+        <Link to={`/Editproduct`} style={{textDecoration:"none",color:"white"}}>Editproduct</Link>
+        <Link to={`/Deleteproduct`} style={{textDecoration:"none",color:"white"}}>Deleteproduct</Link>
+      </div>
+
+      <AllRoutes/>
+      
+    </div>
+  )
+}
+
+export default App;
