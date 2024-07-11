8e4e5aea88ea445d9f514119d876318dabdb063b
SujitSingh521 <5215ssingh@gmail.com>
2024-07-11 22:13:07 +0530

    Integrate-Bootstrap

--- Integrate Bootstrap template/react_project_day-3/README.md
+++ Integrate Bootstrap template/react_project_day-3/README.md
@@ -1,3 +1,7 @@
+Name : Sujit Singh
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +72,10 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+
+
+
+My Presentation Video Link:- https://screenrec.com/share/1IALf9F3Rp
--- Integrate Bootstrap template/react_project_day-3/src/component/Header.jsx
+++ Integrate Bootstrap template/react_project_day-3/src/component/Header.jsx
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faFacebook,faTwitter,faInstagram,faGithub,} from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faTwitter, faInstagram, faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function Header() {
+function Header(){
   return (
     <section
       className="header_css text-light bg-dark p-5 mt-0 text-center text-sm-start"
@@ -10,49 +10,42 @@ function Header() {
     >
       <div className="container py-5">
         <div className="d-sm-flex align-items-center justify-content-between py-5">
-          <div className="cd-intro">
-            <h1 className="cd-headline rotate-1">
-              <span className="cd-words-wrapper">
-                <div className="center">
-                  <h1>
-                    <em>
-                      I'M {" "}
-                      <b className="is-visible">
-                        
-                        <span className="text-danger">Sujit Singh</span>
-                      </b>
-                    </em>
-                  </h1>
-                </div>
-                <div>
-                  <h3 className="my-4">Full Stack Developer</h3>
-                  <p className="lead my-4">
-                    Lorem Ipsum is simply dummy text of the printing and
-                    typesetting industry. <br />
-                    Lorem Ipsum has been the industry's is no standard dummy
-                    <br />
-                    text ever since the 1500s,
-                  </p>
-                  <button className="btn btn-outline-danger text-light btn-lg">
-                 I'M AVAILABLE
-                  </button>
-                  <div className="mt-3">
-                    <a href="#" className="text-light me-3">
-                      <FontAwesomeIcon icon={faFacebook} size="2x" />
-                    </a>
-                    <a href="#" className="text-light me-3">
-                      <FontAwesomeIcon icon={faTwitter} size="2x" />
-                    </a>
-                    <a href="#" className="text-light me-3">
-                      <FontAwesomeIcon icon={faInstagram} size="2x" />
-                    </a>
-                    <a href="#" className="text-light me-3">
-                      <FontAwesomeIcon icon={faGithub} size="2x" />
-                    </a>
-                  </div>
-                </div>
-              </span>
+          <div className="cd-intro"> 
+            <h1>
+              <em>
+                I'M{" "}
+                <b className="is-visible">
+                  <span className="text-danger">Sujit Singh</span>
+                </b>
+              </em>
             </h1>
+            <div>
+              <h3 className="my-4">Full Stack Developer</h3>
+              <p className="lead my-4">
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting industry. <br />
+                Lorem Ipsum has been the industry's is no standard dummy
+                <br />
+                text ever since the 1500s,
+              </p>
+              <button className="btn btn-outline-danger text-light btn-lg">
+                I'M AVAILABLE
+              </button>
+              <div className="mt-3">
+                <a href="#" className="text-light me-3">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="#" className="text-light me-3">
+                  <FontAwesomeIcon icon={faTwitter} size="2x" />
+                </a>
+                <a href="#" className="text-light me-3">
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a href="#" className="text-light me-3">
+                  <FontAwesomeIcon icon={faGithub} size="2x" />
+                </a>
+              </div>
+            </div>
           </div>
           <a href="#">
             <img
@@ -60,11 +53,9 @@ function Header() {
               className="img-fluid"
               alt="Business Man"
               style={{
-                // maxWidth: "100%",
                 height: "500px",
                 display: "block",
                 margin: "0 auto",
-
               }}
             />
           </a>
